11acf6642e7bbaf4f54a50d0e7e09405bea9ab95
fenny <fenny@fennydeMacBook-Pro-2.local>
2026-08-07 10:43:22 +0800

    Allow configurable site base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ruiying1541.github.io',
-  base: '/design-skills',
+  base: process.env.PUBLIC_BASE_PATH ?? '/design-skills',
   integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()]
